6dcb0c20379d69c40d96f2ff6c2be6d7205db580
James Geddes <james@jamesgeddes.org>
2016-09-15 22:24:05 +0100

    Work on search (random)

--- main.elm
+++ main.elm
@@ -1,4 +1,4 @@
-import Html exposing (Html, Attribute, body, h1, p, button, div, input, text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as App
 import Html.Events exposing (onClick, onInput)
@@ -9,38 +9,102 @@ main =
 
 -- MODEL
 
-type alias Model =
-    { movieTitle : String }   -- The title of the movie
+type alias Movie =
+    { movieTitle : String, releaseYear: Int }
 
+type alias Model =
+    { movieTitle : String,             -- The title of the movie
+      movieList : List Movie           -- Movies looked up by title
+    }
+    
 model : Model
-model = Model ""
+model = Model "" []
 
 
 -- UPDATE
 
-type Msg = Change String | Search  
+type Msg = Update String | Search  
 
 update : Msg -> Model -> Model
          
 update msg model =
     case msg of
-        Change newTitle ->
-            { model | movieTitle = newTitle }
+        Update newTitle ->
+            { model |                                 -- This is record update in Elm
+                  movieTitle = newTitle,
+                  movieList = findMovies newTitle 
+            }
         Search ->
             model
+
+findMovies : String -> List Movie
+findMovies title =
+    []
+    
+
+
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-    body []
-        [
-          h1 [] [text "Streaming service comparison tool" ],
-          p [] [text "Build a list of movies you want to watch and compare streaming services based on your list." ],
-          div []
-              [
-                input [ placeholder "Enter a movie title", onInput Change ] [] ,
-                button [ onClick Search ] [ text "Search" ]
-              ]
-        ]
+    body [] [
+         div [ class "header" ] [
+              h1 [] [text "Streaming service comparison tool" ],
+              p [] [text "Build a list of movies you want to watch and compare streaming services based on your list." ]],
+         div [ class "input-section" ] [
+              input [ placeholder "Enter a movie title", id "searchBox", onInput Update ] [],
+              button [ class "searchButton", onClick Search ] [ text "Search" ]],
+         div [ class "search-results-section" ] [
+              h3 [] [text "Search results"],
+              table [] [
+                   thead [] [
+                        td [] [text "Title"],
+                        td [] [text "Release year"],
+                        td [] [] ],
+                   tbody [ id "searchResultsTableBody" ] [
+                        tr [] [
+                             td [colspan 2] [ em [] [text "None"] ],
+                             td [] [ button [ class "addButton" ] [text "Add"] ]]]]],
+         div [ class "movie-list-section" ] [
+              h3 [] [text "Movie list"],
+              table [] [
+                   thead [] [
+                        tr [] [
+                             td [] [],
+                             td [] [],
+                             td [colspan 2] [text "Netflix"],
+                             td [colspan 2] [text "Amazon"],
+                             td [colspan 2] [text "Hulu"],
+                             td [colspan 2] [text "Vudu"],
+                             td [] [],
+                             td [] [],
+                             td [] [] ],
+                        tr [] [
+                             td [] [text "Title"],
+                             td [] [text "Release year"],
+                             td [] [text "Stream"],
+                             td [] [text "Rent"],
+                             td [] [text "Stream"],
+                             td [] [text "Rent"],
+                             td [] [text "Stream"],
+                             td [] [text "Rent"],
+                             td [] [text "Last updated"],
+                             td [] [],
+                             td [] [] ]],
+                   tbody [ id "movieListTableBody" ] [
+                        tr [] [
+                             td [colspan 11] [ em [] [text "None"] ],
+                             td [] [ button [class "updateButton"] [text "Update"] ],
+                             td [] [ button [class "removeButton"] [text "Remove"] ]]]]],
+             div [ class "footer" ] [
+                  p [] [ text "Powered by ",
+                         a [ href "http://canistream.it" ] [text "CanIStream.It"] ]]]
+        
+                                 
+
+                                 
+
+                                 
+                         
         
